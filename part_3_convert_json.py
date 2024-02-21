35e4218f623eb4aa554c913e2d089401c8d8a2d7
jufacefs <systemerror101326@gmail.com>
2024-02-20 23:02:01 -0500

    changing file directory

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -36,17 +36,6 @@ def make_CCLevelPack(level_pack):
        newLevel.add_field(cc_classes.CCMonsterMovementField(monsters))
 
 
-    #    newLevel=level_pack.CCLevel(level_number = level["level_number"],
-    #                                time = level["time"],
-    #                                num_chips = level["num_chips"],
-    #                                upper_layer = level["upper_layer"],
-    #                                lower_layer = level["lower_layer"],
-    #                                optional_fields = fields)
-
-        #Create a new Game object from the json_data by reading
-        # platform=test_data.Platform(launch_year=game["platform"]["launchyear"],
-        #                             name=game["platform"]["name"])
-        # newGame=test_data.Game(title=game["title"],platform=platform,year=game['Year'])
        CCLevelPack.add_level(newLevel)
 
 
@@ -54,18 +43,18 @@ def make_CCLevelPack(level_pack):
 
 
 
-#Part 2
-input_json_file = "level_pack.json"
 
-### Begin Add Code Here ###
+input_json_file = "jiumanz_cc1.json"
+
+
 
 with open(input_json_file, "r") as reader:
-    #Use the json module to load the data from the file
+
     level_pack= json.load(reader)
  
     output=make_CCLevelPack(level_pack)
 
-    # print(output)
+    print(output)
     dat_file = "/Users/emma/Documents/cmu sem4/programming for game designers/chips_tools/jiumanz_cc1.dat"
     cc_dat_utils.write_cc_level_pack_to_dat(output, dat_file)
     """Writes the given CC dat in binary form to the file
@@ -75,4 +64,3 @@ with open(input_json_file, "r") as reader:
     """
 
 
-### End Add Code Here ###
