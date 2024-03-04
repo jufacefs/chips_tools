d61a7facde3a5c82504813af6bf030be7da0f3cb
jufacefs <systemerror101326@gmail.com>
2024-03-03 23:41:08 -0500

    Update part_3_convert_json.py

--- part_3_convert_json.py
+++ part_3_convert_json.py
@@ -20,31 +20,33 @@ def make_CCLevelPack(level_pack):
     levels=level_pack['levels']
     for level in levels:
        newLevel=cc_classes.CCLevel()
-       newLevel.level_number = level["level_number"]
+       
        newLevel.time = level["time"]
        newLevel.num_chips = level["num_chips"]
        newLevel.upper_layer = level["upper_layer"]
        newLevel.lower_layer = level["lower_layer"]
-
-       newLevel.add_field(cc_classes.CCMapTitleField(level["fields"]["3"]))
-       newLevel.add_field(cc_classes.CCEncodedPasswordField(level["fields"]["6"]))
-       newLevel.add_field(cc_classes.CCMapHintField(level["fields"]["7"]))
+       newLevel.level_number=level["level_number"]
+       newLevel.add_field(cc_classes.CCMapTitleField(level["title"]))
+       intPassword=level["password"]
+       listPassword = [int(str(intPassword)[i:i+3]) for i in range(0, len(str(intPassword)), 3)]
+      
+       newLevel.add_field(cc_classes.CCEncodedPasswordField(listPassword))
+       newLevel.add_field(cc_classes.CCMapHintField(level["hintText"]))
        monsters=[]
-       for monster in level["fields"]["10"]:
+       for monster in level["monsters"]:
           newmonstercoords=cc_classes.CCCoordinate(x=monster["x"],y=monster["y"])
           monsters.append(newmonstercoords)
        newLevel.add_field(cc_classes.CCMonsterMovementField(monsters))
 
 
        CCLevelPack.add_level(newLevel)
-
-
     return CCLevelPack
 
 
 
 
-input_json_file = "jiumanz_cc1.json"
+
+input_json_file = "test_map.json"
 
 
 
@@ -55,7 +57,7 @@ with open(input_json_file, "r") as reader:
     output=make_CCLevelPack(level_pack)
 
     print(output)
-    dat_file = "/Users/emma/Documents/cmu sem4/programming for game designers/chips_tools/jiumanz_cc1.dat"
+    dat_file = "/Users/emma/Documents/cmu sem4/programming for game designers/chips_tools/test_map.dat"
     cc_dat_utils.write_cc_level_pack_to_dat(output, dat_file)
     """Writes the given CC dat in binary form to the file
     Args:
